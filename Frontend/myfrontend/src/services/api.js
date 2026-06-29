850a5eb7d2422c67f4ebd015fae5033b026340e1
DayanaA-2109 <dainydayana21@gmail.com>
2026-06-29 17:53:57 +0530

    updated

--- Frontend/myfrontend/src/services/api.js
+++ Frontend/myfrontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',  // ← Change from 3000 to 8000
+  baseURL: 'http://localhost:3000/api/',  // ← Change from 3000 to 8000
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
